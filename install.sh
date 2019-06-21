6bbded82942ea5ba5fa75527ca789dc940676622
Scott Goodrow <sgoodrow@gmail.com>
2019-06-21 10:22:46 -0700

    Some additions

--- install.sh
+++ install.sh
@@ -3,15 +3,21 @@
 BASEDIR=$PWD
 
 function symlink() {
-	ln -sf ${PWD}/${1} ~/.${1}
+	ln -sf ${PWD}/${1} ~/.${2}
 }
 
 cd $BASEDIR/src
 for i in $( ls $PWD ); do
-    symlink $i
+    symlink $i $i
 done
 
 git submodule init
 git submodule update
+
+# symlink gitalias
 cd $BASEDIR/gitalias
-symlink gitalias.txt
+symlink gitalias.txt gitalias.txt
+
+# symlink maven-bash-completion
+cd $BASEDIR/maven-bash-completion
+symlink bash_completion.bash maven_bash_completion.bash
