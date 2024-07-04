0455159d9b492e377e89bcb703ce56a5a9cf1da8
Scott Goodrow <scott@Scotts-Air.localdomain>
2024-07-04 00:00:34 -0700

    add brew

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the current directory of the script
-SCRIPT_DIR=$(PWD)
+DOTFILES_DIR=$(PWD)
 
 # Function to create symlink and handle existing files
 create_symlink() {
@@ -22,7 +22,7 @@ create_symlink() {
 }
 
 # Create symlinks for all files in home directory
-for HOME_FILE in "$SCRIPT_DIR/home/"*; do
+for HOME_FILE in "$DOTFILES_DIR/home/"*; do
     FILENAME=$(basename "$HOME_FILE")
     DEST="$HOME/.$FILENAME"
 
@@ -30,11 +30,34 @@ for HOME_FILE in "$SCRIPT_DIR/home/"*; do
     create_symlink "$HOME_FILE" "$DEST"
 done
 
+# Function to install Brew packages from Brewfile
+install_brew_packages() {
+    local BREWFILE="$DOTFILES_DIR/brew/Brewfile"
+    echo "Installing Brew packages from $BREWFILE..."
+    brew bundle install --file="$BREWFILE"
+}
+
+# Install Brew packages from Brewfile
+install_brew_packages
+
 # Specific handling for VSCode settings
-VS_SETTINGS="$SCRIPT_DIR/app/vscode-settings.json"
+VS_SETTINGS="$DOTFILES_DIR/app/vscode-settings.json"
 VS_SETTINGS_DEST="$HOME/Library/Application Support/Code/User/settings.json"
 
 # Create symlink and handle existing VSCode settings file
 create_symlink "$VS_SETTINGS" "$VS_SETTINGS_DEST"
 
-echo "Symlinks created successfully."
+# Function to install cron job for updating Brewfile
+install_cron_job() {
+    local SCRIPT_PATH="$DOTFILES_DIR/bin/update_brewfile.sh"
+    local CRON_COMMAND="0 0 * * * /bin/bash $SCRIPT_PATH"
+
+    # Add the cron job
+    (crontab -l ; echo "$CRON_COMMAND") | crontab -
+    echo "Cron job installed to update Brewfile daily."
+}
+
+# Install cron job to update Brewfile daily
+install_cron_job
+
+echo "Symlinks and Brew packages installed successfully. Cron job installed."
